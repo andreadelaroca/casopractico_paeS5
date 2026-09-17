cb65090fdd53e001e7c31c2826559d9a22b2c734
Andrea de la Roca <asodelaroca@uamv.edu.ni>
2026-09-17 13:26:41 -0600

    fix: login métodos

--- src/main/java/ni/edu/uam/casopractico/controller/LoginController.java
+++ src/main/java/ni/edu/uam/casopractico/controller/LoginController.java
@@ -1,6 +1,7 @@
 package ni.edu.uam.casopractico.controller;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
@@ -21,43 +22,53 @@ public class LoginController {
     @FXML
     private Button iniciarSesionButton;
     @FXML
-            private Button salirButton;
-
-    Stage stage = (Stage) iniciarSesionButton.getScene().getWindow();
+    private Button salirButton;
+    private int intentos = 0;
 
     @FXML
-    private void iniciarSesion() {
+    private void handleIniciarSesion() {
         String user = usuarioTextField.getText().trim();
-        String pass = pass.getText().trim();
-        int intentos = 0;
+        String pass = passwordField.getText().trim();
 
         while (intentos < 3) {
-            if (user.isEmpty() || pass.getText().isEmpty()) {
+            if (user.isEmpty() || pass.isEmpty()) {
                 advertencia("Campos incompletos", "Debe completar todos los campos.");
                 limpiar();
-                intentos++;
                 return;
-            } else if (user.equals("admin") && pass.equals("1234")) {
+            }
+            if (user.equals("admin") && pass.equals("1234")) {
                 informacion("Operación exitosa", "Ha iniciado sesión exitosamente.");
                 abrirMenu();
             }
             else {
-                error("Credenciales incorrectas", "Credenciales incorrectas, vuelva a intentarlo.");
-                limpiar();
                 intentos++;
+                if (intentos >= 3) {
+                    informacion("Intentos sobrepasados", "Ha intentado entrar al sistema 3 veces. Cerrando ventana.");
+                    cerrarVentana();
+                }
+                else {
+                    error("Credenciales incorrectas", "Credenciales incorrectas, vuelva a intentarlo.");
+                    limpiar();
+                }
             }
         }
-        if (intentos == 3) {
-            informacion("Intentos sobrepasados", "Ha intentado entrar al sistema 3 veces. Cerrando ventana.");
-            stage.close();
-        }
+    }
+
+
+    @FXML
+    private void handleSalir(ActionEvent event) {
+        cerrarVentana();
     }
 
     private void abrirMenu() {
         abrirVentana("/ni/edu/uam/view/MainMenuView.fxml", "Menú Principal");
-        stage.close();
+        cerrarVentana();
     }
 
+    private void cerrarVentana() {
+        Stage stage = (Stage) iniciarSesionButton.getScene().getWindow();
+        stage.close();
+    }
     private void limpiar() {
         usuarioTextField.clear();
         passwordField.clear();
